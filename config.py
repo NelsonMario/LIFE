a0720d401ac7b1169a4b97cc35ff16eef8656cf5
NelsonMario <43506388+NelsonMario@users.noreply.github.com>
2020-09-26 11:11:46 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
-# We currently recommend using Python 3.5
+# We crrently recommend using Python 3.5
 
-username = "nelson.latf@gmail.com" # Here enter your API user name
-password = "y8FAn6t2LNj37YkPa" # Here enter your API password
+#register account in https://apimedic.com/
+username = "" # Here enter your API user name
+password = "" # Here enter your API password
 priaid_authservice_url = "https://sandbox-authservice.priaid.ch/login" # Be aware that sandbox link is for testing pourpose (not real data) once you get live access you shold use https://authservice.priaid.ch/login
 priaid_healthservice_url = "https://sandbox-healthservice.priaid.ch" # Be aware that sandbox link is for testing pourpose (not real data) once you get live access you shold use https://healthservice.priaid.ch
 language = "en-gb" # en-gb, de-ch, fr-fr, it-it, es-es, ar-sa, ru-ru, tr-tr, sr-sp, sk-sk...
